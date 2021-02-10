4bd1f5b9b53caa5bd9bcb830aa49fb9d1f12c759
orv0670 <orv0670@gmail.com>
2021-02-10 23:35:05 +0000

    primera version

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,89 @@
 import React from "react";
+import { element } from "prop-types";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+export class Home extends React.Component {
+	constructor() {
+		super();
+		this.audio = null;
 
-//create your first component
-export function Home() {
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+		this.state = {
+			currentIndex: 1,
+			songs: []
+		};
+	}
+
+	componentDidMount() {
+		this.pauseButton.style.display = "none";
+		fetch("https://assets.breatheco.de/apis/sound/songs")
+			.then(response => response.json())
+			.then(songs => this.setState({ songs }));
+	}
+
+	changeTrack(i) {
+		this.setState({ currentIndex: i });
+		this.audio.current.pause();
+		this.audio.current.load();
+		this.audio.current.play();
+	}
+
+	play = i => {
+		var url = this.state.songs[i].url;
+		const songUrl = "https://assets.breatheco.de/apis/sound/" + url;
+		this.audio.src = songUrl;
+		this.audio.play();
+		this.playButton.style.display = "none";
+		this.pauseButton.style.display = "inline-block";
+		this.setState({ currentIndex: i });
+	};
+
+	pause = () => {
+		this.audio.pause();
+		this.playButton.style.display = "inline-block";
+		this.pauseButton.style.display = "none";
+	};
+
+	render() {
+		const liList = this.state.songs.map((song, index) => {
+			return (
+				<li key={index} onClick={() => this.play(index)}>
+					<span>{index + 1} </span>
+					<span>{song.name}</span>
+				</li>
+			);
+		});
+
+		const audioPlayer = (
+			<>
+				<div className="audioPlayer">
+					<button
+						onClick={() => this.play(this.state.currentIndex - 1)}>
+						<i className="fa fa-caret-left" aria-hidden="true" />
+					</button>
+					<button
+						ref={element => (this.playButton = element)}
+						onClick={() => this.play(this.state.currentIndex)}>
+						<i className="fa fa-play" aria-hidden="true" />
+					</button>
+					<button
+						ref={element => (this.pauseButton = element)}
+						onClick={() => this.pause(this.state.currentIndex)}>
+						<i className="fa fa-pause" aria-hidden="true" />
+					</button>
+					<button
+						onClick={() => this.play(this.state.currentIndex + 1)}>
+						<i className="fa fa-caret-right" aria-hidden="true" />
+					</button>
+				</div>
+
+				<audio ref={element => (this.audio = element)} />
+			</>
+		);
+
+		return (
+			<>
+				{liList}
+				{audioPlayer}
+			</>
+		);
+	}
 }
